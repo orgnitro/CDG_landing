0d53b74fbea589e1a6db7418f81f3fd7653698c5
Semyon Dokiichuk <orgnitro@gmail.com>
2021-02-05 13:37:24 +0200

    index.php as the root

--- contactform.php
+++ contactform.php
@@ -3,15 +3,29 @@
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
   $mailFrom = "kumsingh78@gmail.com";
-  $mailTo = $_POST['email'];
 
   $headers = "Hello, ".$name."\n\n"."Thank You For Visiting My Website";
   $message = "Hi There";
   $subject = "New Message From Fake COMME des GARÇONS";
-
-
-  mail($mailTo, $subject, $message, $headers);
-  header("Location: index.php?mailsend");
+  
+  if (empty($name) || empty($mailTo)) 
+  {
+    header('location: index.php?error');
+  }
+  else 
+  {
+    $to = $_POST['email'];
+    
+    if( mail($to, $subject, $message, $headers) ) 
+    {
+      header('location:index.php?success');
+    }
+  }
+  // header("Location: index.php?mailsend");
+}
+else
+{
+  header('location:index.php');
 }
 
 ?>
