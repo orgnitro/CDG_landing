abb639b6cd5f1d91d3f643f36d5378cbb5ea1ad4
Semyon Dokiichuk <orgnitro@gmail.com>
2021-02-06 22:07:38 +0200

    blabla

--- index.php
+++ index.php
@@ -1,164 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>COMME des GARÇONS parfums</title>
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-  <link rel="manifest" href="/site.webmanifest">
-  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="theme-color" content="#ffffff">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="assets/css/simple-lightbox.min.css">
-  <link rel="stylesheet" href="assets/css/style.css">
+    <meta charset="utf-8" />
+    <title> Создание формы обратной связи </title>
+    <meta name="description" content="Создание формы обратной связи" />
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&display=swap&subset=cyrillic" rel="stylesheet">
+    
 </head>
 
-<!-- Navigation -->
+<body>
 
-<body id="home">
-  <nav class="navbar">
-    <div class="container">
-      <div class="logo"><img src="assets/img/logo.png" alt="COMME des GARÇONS parfums">
-      </div>
-      <ul class="nav">
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#gallery">Gallery</a></li>
-      </ul>
+    <div class="form-wrapper">
+
+        <form class="contact-form" id="contact-form_1" method="POST" enctype="multipart/form-data">
+            <p class="contact-form__title">Оставьте заявку на расчет стоимости</p>
+            <p class="contact-form__description"></p>
+            <div class="contact-form__input-wrapper">
+                <input name="name" type="text" class="contact-form__input contact-form__input_name"
+                    placeholder="Введите ваше имя">
+                <div class="contact-form__error contact-form__error_name"></div>
+            </div>
+
+            <div class="contact-form__input-wrapper">
+                <input name="tel" type="tel" class="contact-form__input contact-form__input_tel"
+                       placeholder="Введите ваш телефон">
+                <div class="contact-form__error contact-form__error_tel"></div>
+            </div>
+
+            <div class="contact-form__input-wrapper"> 
+                <input name="email" type="email" class="contact-form__input contact-form__input_email"
+                    placeholder="Введите ваш email">
+                <div class="contact-form__error contact-form__error_email"></div>
+            </div>
+
+            <div class="contact-form__input-wrapper">
+                <textarea name="text" class="contact-form__input contact-form__text" placeholder="Введите ваше сообщение"></textarea>
+                <div class="contact-form__error contact-form__error_text"></div>
+            </div>
+
+            <div class="contact-form__input-wrapper">
+                <input class="contact-form__input contact-form__file" type="file" name="files[]">
+                <div class="contact-form__error contact-form__error_file"></div>
+            </div>
+
+            <div class="contact-form__input-wrapper">
+                <input type="checkbox" name="agreement" class="contact-form__input contact-form__checkbox" id="agreement">
+                <label for="agreement" class="contact-form__checkbox-label">Я принимаю условия <a href="#">пользовательского соглашения</a></label>
+                <div class="contact-form__error contact-form__error_agreement"></div>
+            </div>
+
+            <button class="contact-form__button" type="submit"> Получить прайс </button>
+        </form>
+        
     </div>
-  </nav>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <!-- jQuery Mask Plugin -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+    
+    <script src="/mail/js/mail.js"></script>
 
-<!-- Section A -->
-
-    <section class="section-a">
-      <div class="container">
-        <div class="left">
-          <img src="assets/img/rouge_bottle.png" alt="Rouge bottle">
-        </div>
-        <div class="right">
-          <div class="rouge-header">
-            <h1>ROUGE</h1>
-            <h3>COMME des GARÇONS</h3>
-          </div>
-          <div class="rouge-description"></div>
-            <p>RELIGIOUS FERVOUR AND EARTHEN REALITY <br>
-              THE TRANSCENDENTAL AND THE TERRESTRIAL.</p>
-              <a href="https://shop.doverstreetmarket.com/comme-des-garcons/parfums/comme-des-garcons-rouge-eau-de-parfum-100ml-natural-spray" 
-                class="btn">Buy now at the DSM
-              </a>
-        </div>
-      </div>
-    </section>
-
-<!-- Section B -->
-
-    <section class="section-b" id="about">
-      <h2>ROUGE, Eau de parfum</h2>
-      <div class="container">
-        <div class="left">
-          <p>The transcendental & terrestrial. A ceremonial clash of contrasts. A radical revelation of blazing harmonies Associations of the colour rouge seen and subverted through the distinctly disruptive gaze of COMME des GARÇONS.</p>
-          <ul>
-            <li>BRIGHT: Crushed clusters of spicy sweet pink peppercorns. The enlightening intensity of Indonesian ginger.</li>
-            <li>FRESH: Earthen emissions from fresh sliced beetroot. Egyptian Geranium leaves, minty and metallic.</li>
-            <li>DEEP: The resinous romance of sticky incense. Woody patchouli dosed with the aromatic amber of cistus.</li>
-          </ul>
-        </div>
-        <div class="right">
-          <img src="assets/img/bottle_box.png" alt="Rouge bottle">
-        </div>
-      </div>
-    </section>
-
-<!-- Section C -->
-
-    <section class="section-c" id="gallery">
-      <!-- <div class="container"> -->
-      <div class="gallery">
-        <a href="assets/img/by_hemingway.jpg" class="big">
-          <img src="assets/img/by_hemingway.jpg" alt="Rouge by Jordan Hemingway">
-        </a>
-
-        <a href="assets/img/with_bg.jpg" class="big">
-          <img src="assets/img/with_bg.jpg" alt="Rouge bottle photo with woman face">
-        </a>
-
-        <a href="assets/img/woman_face.jpg" class="big">
-          <img src="assets/img/woman_face.jpg" alt="Rouge bottle photo with woman face">
-        </a>
-
-        <a href="assets/img/Rouge_box.jpg" class="big">
-          <img src="assets/img/Rouge_box.jpg" alt="Rouge box">
-        </a>
-
-        <a href="assets/img/in_red1.jpg" class="big">
-          <img src="assets/img/in_red1.jpg" alt="Rouge bottle in red 1">
-        </a>
-
-        <a href="assets/img/in_red2.jpg" class="big">
-          <img src="assets/img/in_red2.jpg" alt="Rouge bottle in red2">
-        </a>
-
-        <a href="assets/img/in_red3.jpg" class="big">
-          <img src="assets/img/in_red3.jpg" alt="Rouge bottle in red 3">
-        </a>
-      </div>
-    <!-- </div> -->
-    </section>
-
-<!-- Footer -->
-
-    <footer class="section-footer">
-      <div class="container">
-        <div>
-          <h3>Terms of Service</h2>
-          <p>I Don't Belong to CDG Company. If You Are Interested In Their Products, Better Check Official Pages of CDG perfumery</p>
-          <a href="https://www.comme-des-garcons-parfum.com/"><img src="assets/img/CDG_icon.png" alt="COMME des GARÇONS"></a>
-          <a href="https://www.instagram.com/commedesgarconsparfums/"><img src="assets/img/instagram_icon.png" alt="Instagram icon"></a>
-        </div>
-        <div>
-          <h3>Here You  Can Find Me</h3>
-          <ul>
-            <li><a href="mailto:orgnitro@gmail.com">Email Me</a></li>
-            <li><a href="https://github.com/orgnitro">My Github Page</a></li>
-            <li><a href="https://www.linkedin.com/in/orgnitro/">My LinkedIn Accout</a></li>
-          </ul>
-        </div>
-        <div>
-          <form class="contact-form" id="contact-form_1" method="POST">
-          <h3>Subscribe</h3>
-          <p>You'll get an additional info about COMME des GARÇONS (and with my CV too)</p>
-            <div class="input-wrapper">
-              <input name="name" type="text" class="contact-form__input contact-form__input_name"
-                  placeholder="Your Name">
-              <div class="contact-form__error contact-form__error_name"></div>
-            </div>
-
-            <div class="input-wrapper"> 
-              <input name="email" type="email" class="contact-form__input contact-form__input_email"
-                  placeholder="Your email">
-              <div class="contact-form__error contact-form__error_email"></div>
-            </div>
-
-            <div class="submit-wrapper">
-              <p class="contact-form__description"></p>
-              <button class="contact-form__button" type="submit">Send</button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </footer>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/1.17.2/simple-lightbox.min.js"></script>
-  <script>
-    $(function() {
-        const $gallery = $('.gallery a').simpleLightbox();
-      });
-  </script>
-      <script src="/mail/js/mail.js"></script>
+    <script>
+        $(function() {
+            $('.contact-form__input_tel').mask('+3(000)000-00-00');
+        });
+    </script>
 </body>
+
 </html>
