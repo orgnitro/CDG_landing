f276d30e31ea012858fbbb141f06371eb6012ca1
Semyon Dokiichuk <orgnitro@gmail.com>
2021-02-07 18:46:12 +0200

    responsive design

--- mail/php/mail.php
+++ mail/php/mail.php
@@ -26,8 +26,9 @@
 			$mail = new PHPMailer;
 		}
 
-		$text = '<span>Thank you for visiting my tribute page to CDG perfumery. </p><br>Read more about COMME des GARCONS Rouge at their <a href="https://www.comme-des-garcons-parfum.com/perfumes/rouge">Website</a>. If you liked the images on the webpage, find more at the <a href="https://www.instagram.com/jordan_hemingway/?hl=en">Jordan Hemingway\'s instagram.</a><br> My CV file is always <a href="https://www.dropbox.com/s/j332lh684cmbop8/Dokiichuk_CV_fe.pdf?dl=0">here</a>.<br><br> Good luck, <br>Orgnitro.</span>';
-		// $text = 'hey';
+		// $text = '<span>Thank you for visiting my tribute page to CDG perfumery. </p><br>Read more about COMME des GARCONS Rouge at their <a href="https://www.comme-des-garcons-parfum.com/perfumes/rouge">Website</a>. If you liked the images on the webpage, find more at the <a href="https://www.instagram.com/jordan_hemingway/?hl=en">Jordan Hemingway\'s instagram.</a><br> My CV file is always <a href="https://www.dropbox.com/s/j332lh684cmbop8/Dokiichuk_CV_fe.pdf?dl=0">here</a>.<br><br> Good luck, <br>Orgnitro.</span>';
+		$text = '<span>Thank you for visiting my tribute page to CDG perfumery. </p><br>Read more about COMME des GARCONS Rouge at their <a href="https://www.comme-des-garcons-parfum.com/perfumes/rouge">Website</a>. If you liked the images on the webpage, find more at the <a href="https://www.instagram.com/jordan_hemingway/?hl=en">Jordan Hemingway\'s instagram.</a><br> My CV file is always.<br><br> Good luck, <br>Orgnitro.</span>';
+
 		$mail->setFrom(SENDER);
     $mail->addAddress($email);
     $mail->CharSet = CHARSET;
